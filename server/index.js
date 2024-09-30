f4c7dee66436a4d1e655dfd75aaf534d4618c354
Madan <madanmohan7272906844@gmail.com>
2024-10-01 01:41:57 +0530

    .

--- server/index.js
+++ server/index.js
@@ -27,11 +27,11 @@ app.use("/auth", Auth)
 app.use("/users", UserRouter)
 app.use("/posts", PostRouter)
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // Catch-all handler to send index.html for any other route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
