c4ee33178dadd2754c8d88709416018609152976
Madan <madanmohan7272906844@gmail.com>
2024-10-04 19:04:57 +0530

    .

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,16 +1,17 @@
 import express from "express";
 import { AddComment, AddRemoveLikes, CreatePost, DeleteComment, DeletePost, GetAllPosts, GetUserPosts } from "../controller/posts.js";
 import { VerifyToken } from "../middleware/VerifyToken.js";
+import { version } from "mongoose";
 
 const router = express.Router()
 
-router.get("/allPosts", GetAllPosts)
+router.get("/allPosts", VerifyToken, GetAllPosts)
 
 router.get("/:UserId", VerifyToken, GetUserPosts)
 
-router.post("/createPost/:UserId", CreatePost)
+router.post("/createPost/:UserId", VerifyToken, CreatePost)
 
-router.patch("/deletePost/:PostId", DeletePost)
+router.patch("/deletePost/:PostId", VerifyToken, DeletePost)
 
 router.patch("/:UserId/UpdateLike/:PostId", AddRemoveLikes)
 
